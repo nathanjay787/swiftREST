d4d1a64218a802bfa8e09f2b15f437d605f8f73d
nathanjay787 <nathanjay787@gmail.com>
2017-03-23 23:13:30 -0400

    Starting for real

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,3 +1,25 @@
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+let names = ["accounts":[
+    ["username":"cbcdiver", "email":"c@d.com", "password":"pencil99"],
+    ["username":"dogs", "email":"a@b.com", "password":"pencil99"]
+]]
+
+let server = HTTPServer()
+server.serverPort = 8080
+server.documentRoot = "webroot"
+
+var routes = Routes()
+
+server.addRoutes(routes)
+
+do {
+    try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+    print("Network error thrown: \(err) \(msg)")
+}
 Import Foundation
 import PerfectLib
 import PerfectHTTP
