116543fe79d4524076b4be00a4044a7ee1710644
nathanjay787 <nathanjay787@gmail.com>
2017-03-24 00:23:59 -0400

    clean class for testing

--- Sources/Requests/Get.swift
+++ Sources/Requests/Get.swift
@@ -5,15 +5,15 @@ import Foundation
 
 public class Get {
     class func getAll(request: HTTPRequest, _ response: HTTPResponse) {
-        do {
-            try response.setBody(json: names)
-                response.setHeader(.contentType, value: "application/json")
-                response.completed()
+        //do {
+        //    try response.setBody(json: names)
+        //        response.setHeader(.contentType, value: "application/json")
+        //        response.completed()
         
-        } 
-        catch {
-            response.setBody(string: "Error handling request: \(error)")
-            response.completed()
-        }
+        //} 
+        //catch {
+        //    response.setBody(string: "Error handling request: \(error)")
+        //    response.completed()
+        //}
     }
 }
