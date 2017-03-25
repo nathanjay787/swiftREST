09202c9ab3f74d44813ac0ba657755239a14eb4d
nathanjay787 <nathanjay787@gmail.com>
2017-03-24 22:05:32 -0400

    Gets work, need cleanup

--- Sources/Get.swift
+++ Sources/Get.swift
@@ -5,6 +5,12 @@ import Foundation
 
 public class Get {
     //init(){}
+    //let names = ["accounts":
+    //    [
+    //        ["username":"cbcdiver", "email":"c@d.com", "password":"pencil99"],
+    //        ["username":"dogs", "email":"a@b.com", "password":"pencil99"]
+    //    ]
+    //]
     class func getAll(request: HTTPRequest, _ response: HTTPResponse) {
         do {
             try response.setBody(json: names)
@@ -19,15 +25,45 @@ public class Get {
     }
 
     class func getUser(request: HTTPRequest, _ response: HTTPResponse) {
-        do {
-            try response.setBody(json: names)
-                response.setHeader(.contentType, value: "application/json")
-                response.completed()
-        
-        } 
-        catch {
-            response.setBody(string: "Error handling request: \(error)")
-            response.completed()
+        if let targetUser = request.urlVariables["username"]{
+            for i in 0..<names["accounts"]!.count {
+                if (targetUser == names["accounts"]![i]["username"]!) {
+                    var toReturn = ["accounts": names["accounts"]![i]]
+                    do {
+                        try response.setBody(json: toReturn)
+                        response.setHeader(.contentType, value: "application/json")
+                        response.completed()
+                    }
+                    catch {
+                        response.setBody(string: "Error handling request: \(error)")
+                        response.completed()
+                    }
+                    break
+                }
+            }
+        }
+    }
+
+    class func getUserPassword(request: HTTPRequest, _ response: HTTPResponse) {
+        if let targetUser = request.urlVariables["username"], let targetPassword = request.urlVariables["password"]{
+            for i in 0..<names["accounts"]!.count {
+                if (targetUser == names["accounts"]![i]["username"]!) {
+                    var toReturn = ["result": "false"]
+                    if (targetPassword == names["accounts"]![i]["password"]!){
+                        toReturn = ["result": "true"]
+                    }
+                    do {
+                        try response.setBody(json: toReturn)
+                        response.setHeader(.contentType, value: "application/json")
+                        response.completed()
+                    }
+                    catch {
+                        response.setBody(string: "Error handling request: \(error)")
+                        response.completed()
+                    }
+                    break
+                }
+            }
         }
     }
 }
--- Sources/RouteMaker.swift
+++ Sources/RouteMaker.swift
@@ -7,5 +7,6 @@ class RouteMaker {
     class func makeAllRoutes(theRoutes : inout Routes) {
         theRoutes.add(method: .get, uri: "/json/all", handler: Get.getAll)
         theRoutes.add(method: .get, uri: "/json/username/{username}", handler: Get.getUser)
+        theRoutes.add(method: .get, uri: "/json/username/{username}/{password}", handler: Get.getUserPassword)
     }
 }
